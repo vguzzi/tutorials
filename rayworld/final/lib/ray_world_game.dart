5d1f72af552e1e5f7ebffd2e5022742e6a22323f
Vincenzo Guzzi <vincenzog@spotify.com>
2021-09-05 09:20:55 +0200

    Adds ongoing project based from the starter one

--- rayworld/final/lib/ray_world_game.dart
+++ rayworld/final/lib/ray_world_game.dart
@@ -1,7 +1,10 @@
+import 'dart:collection';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/keyboard.dart';
+import 'package:flutter/services.dart';
 
 import 'components/player.dart';
 import 'components/world.dart';
@@ -11,9 +14,10 @@ import 'package:flutter/material.dart';
 
 import 'helpers/map_loader.dart';
 
-class RayWorldGame extends BaseGame with HasCollidables {
+class RayWorldGame extends BaseGame with HasCollidables, KeyboardEvents {
   final Player _player = Player();
   final World _world = World();
+  final Queue<Direction> _directionStack = Queue<Direction>();
 
   @override
   Future<void> onLoad() async {
@@ -35,7 +39,7 @@ class RayWorldGame extends BaseGame with HasCollidables {
       });
 
   void onJoypadDirectionChanged(Direction direction) {
-    _player.onJoypadDirectionChanged(direction);
+    _player.direction = direction;
   }
 
   WorldCollidable createWorldCollidable(Rect rect) {
@@ -44,5 +48,46 @@ class RayWorldGame extends BaseGame with HasCollidables {
     collidable.width = rect.width;
     collidable.height = rect.height;
     return collidable;
+  }
+
+  @override
+  void onKeyEvent(RawKeyEvent event) {
+    final isKeyDown = event is RawKeyDownEvent;
+    final keyLabel = event.data.keyLabel;
+    Direction? keyDirection;
+
+    switch (keyLabel) {
+      case 'UIKeyInputUpArrow':
+        keyDirection = Direction.up;
+        break;
+      case 'UIKeyInputLeftArrow':
+        keyDirection = Direction.left;
+        break;
+      case 'UIKeyInputRightArrow':
+        keyDirection = Direction.right;
+        break;
+      case 'UIKeyInputDownArrow':
+        keyDirection = Direction.down;
+        break;
+    }
+
+    if (keyDirection != null) {
+      if (isKeyDown) {
+        _directionStack.addFirst(keyDirection);
+        _player.direction = keyDirection;
+      } else {
+        _directionStack.remove(keyDirection);
+
+        if (_player.direction == keyDirection) {
+          var direction = Direction.none;
+
+          if (_directionStack.isNotEmpty) {
+            direction = _directionStack.first;
+          }
+
+          _player.direction = direction;
+        }
+      }
+    }
   }
 }
